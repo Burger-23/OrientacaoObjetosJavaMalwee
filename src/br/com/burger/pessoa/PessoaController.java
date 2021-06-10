c78bfe2029bf6a03ff0c93a4ceabace81148847a
GabrielBurger23 <gabrielfrancisco2004@gmail.com>
2021-06-10 16:36:49 -0300

    Refatoração da Controller para adicionar pessoas.

--- src/br/com/burger/pessoa/PessoaController.java
+++ src/br/com/burger/pessoa/PessoaController.java
@@ -40,27 +40,65 @@ public class PessoaController {
 		
 		System.out.print("Informe sua altura: ");
 		pessoa.setAltura(tec.nextDouble());
-		System.out.println("\n");
 		
-	
+		System.out.print("Informe o Pais: ");
+		tec.nextLine();
+		pessoa.setNomePais(tec.nextLine());
+		
+		System.out.print("Informe a sigla do Pais: ");
+		pessoa.setSigla(tec.nextLine());
+		
+		System.out.print("Informe o nome do seu Estado: ");
+		pessoa.setNomeEstado(tec.nextLine());
+		
+		System.out.print("Informe a sigla do seu Estado: ");
+		pessoa.setUF(tec.nextLine());
+		
+		System.out.print("Informe o nome da sua Cidade: ");
+		pessoa.setNomeCd(tec.nextLine());
+		
+		System.out.print("Informe o nome da Rua: ");
+		pessoa.setNomeRua(tec.nextLine());
+		
+		System.out.print("Informe o Bairro: ");
+		pessoa.setBairro(tec.nextLine());
+		
+		System.out.print("Informe o número da casa: ");
+		pessoa.setNumero(tec.nextInt());
+		
+		System.out.print("Informe o Complemento: ");
+		pessoa.setComplemento(tec.nextLine());
+
 		return pessoa;
 		
 	}
 	
 	public List<Pessoa> listarPessoas(List<Pessoa> pessoas){
 		System.out.println("\n");
-		System.out.println("---PESSOAS--CADASTRADAS---");
+		System.out.println("-----------PESSOAS---CADASTRADAS-----------");
+		System.out.println("\n");
+
 		
-		System.out.printf ("| %15s | %4s | %5s | %6s |\n", "Nome", "Ano", "Idade", "Altura");
+		System.out.printf ("| %15s | %4s | %5s | %6s | %11s| %4s | %4s | %23s | %30s | %30s | %4s | %30s |\n", "Nome", "Ano", "Idade", "Altura", "Pais", "Sigla do Pais", "Nome do Estado", "Sigla do Estado",
+				"Nome da Cidade", "Nome da Rua", "Bairro", "Número da Casa");
 		for(int i = 0; i < pessoas.size(); i++) {
-			System.out.printf("| %15s | %4d | %5d | %6.2f |\n", 
+			System.out.printf("| %15s | %4d | %5d | %6.2f | %10s | %4s | %15s | %4s | %23s | %30s | %30s | %4s | %30s |\n", 
 					pessoas.get(i).getNome(),
 					pessoas.get(i).getAnoDeNascimento(),
 					pessoas.get(i).getIdade(),
-					pessoas.get(i).getAltura());
+					pessoas.get(i).getAltura(),
+					pessoas.get(i).getNomePais(),
+					pessoas.get(i).getSigla(),
+					pessoas.get(i).getNomeEstado(),
+					pessoas.get(i).getUF(),
+					pessoas.get(i).getNomeCd(),
+					pessoas.get(i).getNomeRua(),
+					pessoas.get(i).getBairro(),
+					pessoas.get(i).getNumero(),
+					pessoas.get(i).getComplemento());
 		}
-		
 		return pessoas;
 	}
  	
-}
+	
+	}	
